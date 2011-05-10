95a28416a0b96c6851fdb019ce86487652c0e983
Fan Jun <powerfj@gmail.com>
2011-05-10 23:47:53 +0800

    fix the binnding url with space problem

--- source/xbl.js
+++ source/xbl.js
@@ -4,7 +4,7 @@
 
 //
 var rCSSRules		= /\s*([^}]+)\s*{([^}]+)}/g,
-	rCSSBindingUrls	= /binding:\s*url\s*\(['"\s]*([^'"]+)['"\s]*\)/g,
+	rCSSBindingUrls	= /binding\s*:\s*url\s*\(\s*(?:(['"])|[^'"])([^\1]+)\1\s*\)/g,
 	rCSSComments	= /(\/\*.*?\*\/)/g,
 	rCSSNameSpaces	= /@namespace\s+(\w+)?\s*"([^"]+)";?/g;
 
